9687d889e59f2a1161d35a632505abb6742281fe
Paulounk <paulo.job@hotmail.com>
2021-09-21 21:02:25 -0300

    Atividade Pratica Concluida

--- Atividades/Simulacao_Financiamento/script.js
+++ Atividades/Simulacao_Financiamento/script.js
@@ -1,44 +1,52 @@
-var valor =  document.querySelector("#valor").value
-var prazo =  document.querySelector("#prazo").value
-var juros =  document.querySelector("#juros").valueAsNumber
-
+var valor = document.querySelector("#valor").value
+var prazo = document.querySelector("#prazo").value
+var juros = document.querySelector("#juros").valueAsNumber
 var prazoMeses = document.querySelector("#prazoMeses")
 var jurosMes = document.querySelector("#jurosMes")
-var jurosAnos = document.querySelector("#jurosAnos")
+var jurosAcum = document.querySelector("#jurosAcum")
 
-//CRIA ELEMENTOS DINAMICOS
-function criaElemento(elemento){
-    return document.createElement(elemento);
-}
+//FUNÇÃO PRINCIPAL
+function simulaFinanciamento() {
 
-function simulaFinanciamento(){
-
-    //PRAZO
+    //CALCULO DO PRAZO
     prazoMeses.value = prazo * 12;
     prazoMeses.innerHTML = prazoMeses.value;
 
-    //JUROS AO MÊS
-    jurosMes.value = ((1 + juros)**(1/12)-1);
+    //CALCULO DO JUROS AO MÊS
+    jurosMes.value = ((1 + juros) ** (1 / 12) - 1);
     jurosMes.innerHTML = jurosMes.value;
 
-    //JUROS ACUMULADOS
-    var vAmortizacao = valor/prazoMeses.value;
+    //CALCULO DA AMORTIZAÇÃO
+    var vAmortizacao = 0;
+    vAmortizacao = (valor/prazoMeses.value).toFixed(2);
+
+    var jAcumulados=0;
+    for(let i = 0; i < prazoMeses.value; i++){
+        //JUROS
+        let resp = 0;
+        resp = parseFloat(((valor - (i * vAmortizacao)) * jurosMes.value).toFixed(2));
+     
+        //TOTAL
+        let vtotal = parseFloat(resp + parseFloat(vAmortizacao)).toFixed(2);
     
-    //ELEMENTOS DINAMICOS
-    
+        //JUROS ACUMULADOS
+        jAcumulados = jAcumulados + resp
 
-    for(var i = 0; i <= prazoMeses.value; i++){
-        
-        var resp  = ((valor - (i * vAmortizacao)) * jurosMes.value).toFixed(2)
-        var tabela = document.querySelector('#tabela')
-        var columns = document.createElement('tr');
-        columns.innerHTML = resp
-        tabela.appendChild(columns)
-       
-    }
+        //CRIANDO ELEMENTOS DINAMICAMENTE
+        var tbody = document.getElementById('tbody')
 
-    
+        let tr = tbody.insertRow()
 
+        let td_pres = tr.insertCell()
+        let td_amort = tr.insertCell()
+        let td_juros = tr.insertCell()
+        let td_total = tr.insertCell()
 
+        td_pres.innerHTML = ((i+1) + "º")
+        td_amort.innerHTML = "  R$ " + vAmortizacao
+        td_juros.innerHTML = "  R$ " + resp
+        td_total.innerHTML = "  R$ " + vtotal
+    }  
+    jurosAcum.value = jAcumulados.toFixed(2);
+    jurosAcum.innerHTML = jAcumulados
 }
-
